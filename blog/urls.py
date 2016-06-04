2d517308d7c6c7944f325f4f296f69ca5e7c93bd
caiquepereira <caiquepereira@gmail.com>
2016-06-04 17:36:07 -0300

    Created login page and redirection to it

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import include, url
 from . import views
+import django.contrib.auth.views
+
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = [
     url(r'^$', views.post_list),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^accounts/login/$', django.contrib.auth.views.login, name='login'),
 ]
