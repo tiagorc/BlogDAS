99bd893c1ff596c9a624fe64c7e081af6ec5a057
caiquepereira <caiquepereira@gmail.com>
2016-06-04 17:50:29 -0300

    Validates if the user is authenticated and redirects on login

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ admin.autodiscover()
 
 urlpatterns = [
     url(r'^$', views.post_list),
+    url(r'^accounts/profile/$', views.post_list),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
