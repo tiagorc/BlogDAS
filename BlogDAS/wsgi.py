08bc0cc0206228e8331042388b0a77be5dde84f7
caiquepereira <caiquepereira@gmail.com>
2016-06-04 10:14:24 -0300

    Configured wsgi as before

--- BlogDAS/wsgi.py
+++ BlogDAS/wsgi.py
@@ -8,14 +8,8 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
-import sys
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
-path = '/home/blogdas/BlogDAS'  # use your own username here
-if path not in sys.path:
-    sys.path.append(path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BlogDAS.settings")
-
-application = DjangoWhiteNoise(get_wsgi_application())
+application = get_wsgi_application()
