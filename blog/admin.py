29f179553ad14a1a000bab34e3b0ee79a6e9d762
caiquepereira <caiquepereira@gmail.com>
2016-06-05 11:45:35 -0300

    Added userProfile model admin to user admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.contrib.auth.models import User
 from .models import Post
 from .models import Comment
+from blog.models import UserProfile
 
 admin.site.register(Post)
 admin.site.register(Comment)
+
+
+class UserProfileInline(admin.TabularInline):
+    model = UserProfile
+
+
+class UserAdmin(DjangoUserAdmin):
+    inlines = (UserProfileInline,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
