286770501ccb3c3bfab7c6fc90877b534fc48b6d
tiagorc <tiagobrasil2005@yahoo.com.br>
2016-06-06 22:51:40 -0300

    basic tests created

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from .models import Post, Comment
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.test.client import Client
+from model_mommy import mommy
+# from model_mommy.recipe import Recipe, foreign_key
+
 
 # Create your tests here.
+class PostFactory:
+    def make_post(self):
+        post = mommy.make(Post)
+        return post
+    def make_post_blank_text(self):
+        return Post.objects.create(text="")
+
+    def make_comment(self):
+        comment = mommy.make(Comment)
+        return comment
+
+class PostTestCase(TestCase):
+    def setUp(self):
+        self.blank_message = PostFactory().make_post()
+
+    def test_make_post(self):
+        # pass
+        post = PostFactory().make_post()
+        self.assertEqual(post.__str__(),post.title)
+
+    def test_make_comment(self):
+        comment = PostFactory().make_comment()
+        self.assertEqual(comment.__str__(),comment.text)
