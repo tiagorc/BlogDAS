d871efb821864121ef58bc18bc2781d1ff8891c4
MaxOliveira <maxwell_oliveira2@hotmail.com>
2016-06-13 17:37:35 -0300

    implementing private data class pattern

--- blog/models.py
+++ blog/models.py
@@ -9,19 +9,56 @@ class TimestampedMixin(models.Model):
         abstract = True
 
 
-class Post(models.Model):
+class PostData(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
     published_date = models.DateTimeField(blank=True, null=True)
 
+    def __init__(self, author, title, text, published_date):
+        self.author = author
+        self.title = title
+        self.text = text
+        self.published_date = published_date
+
+    @property
+    def author(self):
+        return self.author
+    @property
+    def title(self):
+        return self.title
+    @property
+    def text(self):
+        return self.text
+    @property
+    def published_date(self):
+        return self.published_date
+
     def publish(self):
         self.published_date = timezone.now()
         self.save()
 
-    def __str__(self):
-        return self.title
+class Post(models.Model):
+    def __init__(self, author, title, text, published_date):
+        self.post_Data = PostData(author, title, text, published_date)
 
+    def author(self):
+        return self.post_Data.author
+
+    def title(self):
+        return self.post_Data.title
+
+    def text(self):
+        return self.post_Data.text
+
+    def published_date(self):
+        return self.post_Data.published_date
+
+    def publish(self):
+        return self.post_Data.publish
+
+    def __str__(self):
+        return self.post_Data.title
 
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', related_name='comments')
