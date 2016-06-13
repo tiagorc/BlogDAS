a239631362b59397e69e8dd952a28da969a4b0c6
MaxOliveira <maxwell_oliveira2@hotmail.com>
2016-06-13 19:32:19 -0300

    implementing private data class pattern in model comment

--- blog/models.py
+++ blog/models.py
@@ -21,16 +21,15 @@ class PostData(models.Model):
         self.text = text
         self.published_date = published_date
 
-    @property
     def author(self):
         return self.author
-    @property
+
     def title(self):
         return self.title
-    @property
+
     def text(self):
         return self.text
-    @property
+
     def published_date(self):
         return self.published_date
 
@@ -42,36 +41,49 @@ class Post(models.Model):
     def __init__(self, author, title, text, published_date):
         self.post_Data = PostData(author, title, text, published_date)
 
-    def author(self):
-        return self.post_Data.author
-
-    def title(self):
-        return self.post_Data.title
-
-    def text(self):
-        return self.post_Data.text
-
-    def published_date(self):
-        return self.post_Data.published_date
-
     def publish(self):
         return self.post_Data.publish
 
     def __str__(self):
         return self.post_Data.title
 
-class Comment(models.Model):
+class CommentData(models.Model):
     post = models.ForeignKey('blog.Post', related_name='comments')
     author = models.CharField(max_length=200)
     text = models.TextField()
     approved_comment = models.BooleanField(default=False)
 
+    def __init__(self, post, author, text, approved_comment):
+        self.post = post
+        self.author = author
+        self.text = text
+        self.approved_comment = approved_comment
+
+    def post(self):
+        return self.post
+
+    def author(self):
+        return self.author
+
+    def text(self):
+        return self.text
+
+    def approved_comment(self):
+        return self.approved_comment
+
+class Comment(models.Model):
+    def __init__(self, post, author, text, approved_comment):
+        self.comment_data = CommentData(post, author, text, approved_comment)
+
+    def approved_comment(self):
+        return self.comment_data.pproved_comment
+
     def approve(self):
-        self.approved_comment = True
+        self.comment_data.approved_comment = True
         self.save()
 
     def __str__(self):
-        return self.text
+        return self.comment_data.text
 
     def approved_comments(self):
         return self.comments.filter(approved_comment=True)
